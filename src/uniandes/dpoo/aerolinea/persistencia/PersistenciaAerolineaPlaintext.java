bdd7155cc77edd7184929c4fd48c4c3c1e1635ae
Ale81015 <125764674+Ale81015@users.noreply.github.com>
2024-09-11 11:50:52 -0500

    Entrega

--- src/uniandes/dpoo/aerolinea/persistencia/PersistenciaAerolineaPlaintext.java
+++ src/uniandes/dpoo/aerolinea/persistencia/PersistenciaAerolineaPlaintext.java
@@ -1,5 +1,10 @@
 package uniandes.dpoo.aerolinea.persistencia;
 
+import java.io.File;
+import java.nio.file.Files;
+
+import org.json.JSONObject;
+
 import uniandes.dpoo.aerolinea.modelo.Aerolinea;
 
 /**
@@ -8,11 +13,18 @@ import uniandes.dpoo.aerolinea.modelo.Aerolinea;
  * Su objetivo es sólo ilustrar que podría haber varias implementaciones de la misma interfaz, y que durante la ejecución alguien podría decidir cuál de estas implementaciones
  * usar.
  */
+//No supe que hacer acá :(
+
 public class PersistenciaAerolineaPlaintext implements IPersistenciaAerolinea
 {
     @Override
     public void cargarAerolinea( String archivo, Aerolinea aerolinea )
     {
+    	String jsonCompleto = new String( Files.readAllBytes( new File( archivo ).toPath( ) ) );
+        JSONObject raiz = new JSONObject( jsonCompleto );
+
+        cargarClientes( aerolinea, raiz.getJSONArray( "clientes" ) );
+        cargarTiquetes( aerolinea, raiz.getJSONArray( "tiquetes" ) );
         // No está implementado (y no es necesario para el taller)
     }
 
